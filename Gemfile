781c1a8e3755728249fbc7bb2ff213271b4bfe29
Matthew Jording <mjording@opengotham.com>
2011-05-17 01:50:57 -0400

    bringing in dependencies for ReverseProxy need to add spec deps

--- Gemfile
+++ Gemfile
@@ -11,5 +11,7 @@ group :development do
   gem "rspec", "~> 2.3.0"
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.5.2"
+  gem "rack-test"
+  gem "webmock"
   gem "rcov", ">= 0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'rack/test'
 require 'whack-a-node'
-
+require 'rack/reverse_proxy'
+require 'rack/streaming_proxy'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
