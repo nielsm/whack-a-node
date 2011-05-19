ed02d429b0bada48eead946b8939e68339fe9676
Niels Meersschaert <nmeersschaert@mac.com>
2011-05-19 09:32:53 -0400

    Renamed test to match class change

--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'rack/test'
 
-describe "WhackANode" do
+describe "WhackANode::Proxy" do
   include Rack::Test::Methods
   include WebMock::API
   class WhackyTest < WhackANode::Proxy
@@ -11,7 +11,7 @@ describe "WhackANode" do
   end
   
   def app
-     WhackANode.new
+     WhackANode::Proxy.new
   end
 
   #before(:each) do
